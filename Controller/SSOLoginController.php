b7380fbdd18d0d143892dd591be470a749ef8848
llama <sasha205140245@gmail.com>
2018-09-09 23:12:26 +0300

    n6

--- Controller/SSOLoginController.php
+++ Controller/SSOLoginController.php
@@ -3,6 +3,9 @@
 
 namespace Itratos\SingleSignOn\Controller;
 
+use OxidEsales\Eshop\Application\Controller\FrontendController;
+
+
 define("TOOLKIT_PATH", '/home/llama/projects/oxid6/vendor/onelogin/php-saml/');
 require_once(TOOLKIT_PATH . '_toolkit_loader.php');   // We load the SAML2 lib
 require_once __DIR__ . '/../Helper/SSOSamlHelper.php';
