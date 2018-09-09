44715af4ec0595b1113925cf97e8bb1d6e5aca20
llama <sasha205140245@gmail.com>
2018-09-09 14:49:54 +0300

    n2

--- Component/SSOUserComponent.php
+++ Component/SSOUserComponent.php
@@ -5,7 +5,7 @@ namespace Itratos\SingleSignOn\Component;
 
 define("TOOLKIT_PATH", '/home/llama/projects/oxid6/vendor/onelogin/php-saml/');
 require_once(TOOLKIT_PATH . '_toolkit_loader.php');   // We load the SAML2 lib
-require_once __DIR__ . '../Helper/SSOSamlHelper.php';
+require_once __DIR__ . '/../Helper/SSOSamlHelper.php';
 
 class SSOUserComponent extends SSOUserComponent_parent
 {
